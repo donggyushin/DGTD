67dba7ac22cae22b78f9f6e628f9cceaae60642d
신동규 <donggyu9410@gmail.com>
2023-03-05 14:37:52 +0900

    feat: init project

---
+++ DGTD/DGTDApp.swift
@@ -0,0 +1,17 @@
+//
+//  DGTDApp.swift
+//  DGTD
+//
+//  Created by 신동규 on 2023/03/05.
+//
+
+import SwiftUI
+
+@main
+struct DGTDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

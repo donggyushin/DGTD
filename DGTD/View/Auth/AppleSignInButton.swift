f28b26a5ae5bf1007e7e8c77248fb1ec2c37d322
신동규 <donggyu9410@gmail.com>
2023-03-12 01:17:33 +0900

    feat: apple sign in data

--- DGTD/View/Auth/AppleSignInButton.swift
+++ DGTD/View/Auth/AppleSignInButton.swift
@@ -13,7 +13,30 @@ struct AppleSignInButton: View {
         SignInWithAppleButton { request in
             print("dg: request: \(request)")
         } onCompletion: { result in
-            print("dg: result: \(result)")
+            switch result {
+            case .failure(let error):
+                print("dg: error: \(error.localizedDescription)")
+            case .success(let authResults):
+                
+                if let appleIDCredential = authResults.credential as? ASAuthorizationAppleIDCredential {
+                    // 계정 정보 가져오기
+                    let UserIdentifier = appleIDCredential.user
+                    let fullName = appleIDCredential.fullName
+                    let name =  (fullName?.familyName ?? "") + (fullName?.givenName ?? "")
+                    let email = appleIDCredential.email
+                    let IdentityToken = String(data: appleIDCredential.identityToken!, encoding: .utf8)
+                    let AuthorizationCode = String(data: appleIDCredential.authorizationCode!, encoding: .utf8)
+                    
+                    print("dg: UserIdentifier: \(UserIdentifier)")
+                    print("dg: fullName: \(fullName)")
+                    print("dg: name: \(name)")
+                    print("dg: email: \(email)")
+                    print("dg: IdentityToken: \(IdentityToken)")
+                    print("dg: AuthorizationCode: \(AuthorizationCode)")
+                    
+                }
+                    
+            }
         }
     }
 }
